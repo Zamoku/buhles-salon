e2b0aeb642c12c117858e8d8cf1776582e25612b
Nomzamo <nomzamo.nmuleka@gmail.com>
2022-11-24 14:50:00 +0200

    database changes

--- sql/data.sql
+++ sql/data.sql
@@ -20,3 +20,48 @@ INSERT INTO stylist (first_name, last_name,phone_number, commission_percentage) 
 
 -- GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public to zamoe;
 -- GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public to zamoe;
+
+-- Table create scripts here
+
+CREATE TABLE IF NOT EXISTS client (
+
+    id serial not null primary key,
+	first_name text not null,
+    last_name text not null,
+    phone_number varchar(255) not null
+
+);
+
+CREATE TABLE IF NOT EXISTS treatment(
+
+    id serial not null primary key,
+    type text not null,
+    code text not null,
+    price varchar(255) not null
+
+);
+
+CREATE TABLE IF NOT EXISTS stylist(
+
+    id serial not null primary key,
+    first_name text not null,
+    last_name text not null,
+    phone_number varchar(255) not null,
+    commission_percentage float not null
+
+);
+CREATE TABLE IF NOT EXISTS booking(
+
+    id serial not null primary key,
+    booking_date date,
+    booking_time time,
+    client_id int not null,
+    treatment_id int not null,
+    stylist_id int not null,
+    foreign key (client_id) references client(id),
+    foreign key (treatment_id) references treatment(id),
+    foreign key (stylist_id) references stylist(id)
+
+);
+
+
